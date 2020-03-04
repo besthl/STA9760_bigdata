8b028199e29595bb63b6384ee6a93156fc5e5770
besthl <60530352+besthl@users.noreply.github.com>
2020-03-04 11:36:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,44 @@
 from sys import argv
-from src.parkingviolations.api import Service
+from src.OPCV.getdata import Function
 from os import environ
 
 if __name__ == "__main__":
     app_key = environ.get("APP_KEY")
-    # print("APP_KEY={}".format(app_key))
+    #print page_size and num_pages
     page_size_str = argv[1]
     page_size = int(page_size_str.split('=')[1])
     num_pages_str = argv[2]
     num_pages = int(num_pages_str.split('=')[1])
-    # print(f"page_size={page_size}, num_pages={num_pages}")
+    
+    #print output
     try:
-        output_str = argv[3]
+        output = argv[3]
+
     except Exception:
-        output_str = None
+        output = None
     location = 'nc67-uf89'
 
     limit_size = int(page_size / num_pages)
 
-    if output_str is None:
-        with Service(app_key) as service:
-            total_size = service.get_size(location)
+    if output is None:
+        with Function(app_key) as function:
+            total_size = function.get_size(location)
             # print(f"total_size={total_size}")
-            print(service.get_info(location, limit_size))
+            print(function.get_info(location, limit_size))
             offset = 0
             for i in range(num_pages-1):
                 offset += limit_size
                 if offset >= total_size:
                     break;
-                print(service.get_next_info(location, limit_size, offset))
+                print(function.get_next_info(location, limit_size, offset))
     else:
-        output = output_str.split("=")[1]
-        with Service(app_key) as service, open(output, "w") as fw:
-            total_size = service.get_size(location)
-            fw.write(f"{service.get_info(location, limit_size)}\n")
+        output = output.split("=")[1]
+        with Function(app_key) as function, open(output, "w") as fw:
+            total_size = function.get_size(location)
+            fw.write(f"{function.get_info(location, limit_size)}\n")
             offset = 0
             for i in range(num_pages-1):
                 offset += limit_size
                 if offset >= total_size:
                     break;
-                fw.write(f"{service.get_next_info(location, limit_size, offset)}\n")
+                fw.write(f"{function.get_next_info(location, limit_size, offset)}\n")
